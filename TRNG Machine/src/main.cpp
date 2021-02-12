c13c515cda42bf6d92786a30920d48c17808351f
Henry Oliver <henry983615@gmail.com>
2021-02-12 17:17:10 +1300

    tidied up main.cpp

--- TRNG Machine/src/main.cpp
+++ TRNG Machine/src/main.cpp
@@ -72,13 +72,13 @@ void setup() {
 
 
 void loop() {
-  if (constrain(analogRead(keyPin), 0, 1023) == 1023){
-    Serial.println("ON");
-  }
-  else{
-    Serial.println("OFF");
-  }
-  Serial.println(constrain(analogRead(keyPin), 0, 1023));
+  // if (constrain(analogRead(keyPin), 0, 1023) == 1023){
+  //   Serial.println("ON");
+  // }
+  // else{
+  //   Serial.println("OFF");
+  // }
+  // Serial.println(constrain(analogRead(keyPin), 0, 1023));
   
   if (completeSeed()){
     Serial.println("Have complete seed!");
@@ -89,32 +89,14 @@ void loop() {
   
   //Get Deltatime
   unsigned long currentMillis = millis();
-  float t = (currentMillis - previousMillis)/LOG_PERIOD;
-  
-  //Smooth Needle
-  String displayText = String("sV/hr:" + String(sv));
-  DIS_RIGHT.clearBuffer();
+  float deltaTime = (currentMillis - previousMillis)/LOG_PERIOD;
 
-  //targetYPos = pow(((0.08*(targetXPos)) - (64.0*0.08)),2);
-  //Draw gauage
-  float tmpPos = lerp(oldXPos, targetXPos, t);
-  DIS_RIGHT.drawLine(64, 60, tmpPos, pow(((0.08*(tmpPos)) - (64.0*0.08)),2));
-  DIS_RIGHT.setFont(u8g2_font_pressstart2p_8f);  
-  DIS_RIGHT.drawStr(25, 45, displayText.c_str());
-  DIS_RIGHT.drawCircle(64, 60, 6);
-  DIS_RIGHT.drawLine(0, 0, 0, 20);
-  DIS_RIGHT.drawLine(32, 0, 32, 15);
-  DIS_RIGHT.drawLine(64, 0, 64, 10);
-  DIS_RIGHT.drawLine(96, 0, 96, 15);
-  DIS_RIGHT.drawLine(127, 0, 127, 20);
-  DIS_RIGHT.setFont(u8g2_font_5x7_tn );  
-  DIS_RIGHT.drawStr(0, 30, "0");
-  DIS_RIGHT.drawStr(63, 20, "3");
-  DIS_RIGHT.drawStr(123, 30, "6");
-  DIS_RIGHT.sendBuffer();
+  //Smooth Needle
+  smoothGuage(deltaTime);
 
   //Check fingerprint
   int f = matchPrint();
+
   //Correct
   if (f >= 0){
     Serial.println("GOT RET: ");
@@ -129,35 +111,15 @@ void loop() {
   }
 
   //Show seed
-  seedStr = "";
-  for (int y = 0; y < 31; y++){
-    if (seed[y] == seedEmpty){
-      seedStr += '*';
-    }
-    else if (seed[y] == 0x01){
-      seedStr += '1';
-    }
-    else{
-      seedStr += '0';
-    }
-  }
-
+  updateSeedString();
+  
   //Set new info and calc new needle pos
-  if(t >= 1.0){
-    previousMillis = currentMillis;
-    cpm = counts * multiplier;
-    sv = cpm * TUBE_MOD;
-    
-    //update guage
-    updateGuage(sv, true);
-
-    //reset counts
-    counts = 0;
+  if(deltaTime >= 1.0){
+    calcNewGuageInfo(currentMillis);
   }
+  //Draw seed string on left screen
   else{
-    DIS_LEFT.clearBuffer();
-    DIS_LEFT.drawStr(0, 43, seedStr.c_str()); 
-    DIS_LEFT.sendBuffer(); 
+    displaySeedProgress();
   }
 
   //Keypad
--- TRNG Machine/src/radiationEyeCandy.h
+++ TRNG Machine/src/radiationEyeCandy.h
@@ -22,3 +22,60 @@ void updateGuage(float val, bool test){
       DIS_RIGHT.sendBuffer();
     }
 }
+
+//Smooth Needle
+void smoothGuage(float t){
+  String displayText = String("sV/hr:" + String(sv));
+  DIS_RIGHT.clearBuffer();
+
+  //targetYPos = pow(((0.08*(targetXPos)) - (64.0*0.08)),2);
+  //Draw gauage
+  float tmpPos = lerp(oldXPos, targetXPos, t);
+  DIS_RIGHT.drawLine(64, 60, tmpPos, pow(((0.08*(tmpPos)) - (64.0*0.08)),2));
+  DIS_RIGHT.setFont(u8g2_font_pressstart2p_8f);  
+  DIS_RIGHT.drawStr(25, 45, displayText.c_str());
+  DIS_RIGHT.drawCircle(64, 60, 6);
+  DIS_RIGHT.drawLine(0, 0, 0, 20);
+  DIS_RIGHT.drawLine(32, 0, 32, 15);
+  DIS_RIGHT.drawLine(64, 0, 64, 10);
+  DIS_RIGHT.drawLine(96, 0, 96, 15);
+  DIS_RIGHT.drawLine(127, 0, 127, 20);
+  DIS_RIGHT.setFont(u8g2_font_5x7_tn );  
+  DIS_RIGHT.drawStr(0, 30, "0");
+  DIS_RIGHT.drawStr(63, 20, "3");
+  DIS_RIGHT.drawStr(123, 30, "6");
+  DIS_RIGHT.sendBuffer();
+}
+
+void calcNewGuageInfo(unsigned long currentMillis){
+    previousMillis = currentMillis;
+    cpm = counts * multiplier;
+    sv = cpm * TUBE_MOD;
+    
+    //update guage
+    updateGuage(sv, true);
+
+    //reset counts
+    counts = 0;
+}
+
+void updateSeedString(){
+    seedStr = "";
+    for (int y = 0; y < 31; y++){
+        if (seed[y] == seedEmpty){
+            seedStr += '*';
+        }
+        else if (seed[y] == 0x01){
+            seedStr += '1';
+        }
+        else{
+            seedStr += '0';
+        }
+    }
+}
+
+void displaySeedProgress(){
+    DIS_LEFT.clearBuffer();
+    DIS_LEFT.drawStr(0, 43, seedStr.c_str()); 
+    DIS_LEFT.sendBuffer(); 
+}
